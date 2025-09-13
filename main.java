29156726c7346eebd1d95b1d3bbebae26de1d187
Magnus Junior <magnus.antonio.j@gmail.com>
2025-09-13 19:04:41 -0300

    feat: age function added

--- main.java
+++ main.java
@@ -2,13 +2,17 @@ public class main{
     public static void main(String[] args) {
         int average = calculate(5, 10);
         String name = getName();
-        String message = "Hello, " + name + "! The average is: " + average;
+        String message = "Hello, " + name + " your age is " + getAge() + "! The average is: " + average;
         System.out.println(message);
     }
 }
 
 public class getName(){
     return "Steve";
+}
+
+public class getAge(){
+    return 30;
 }
 
 public class calculate(int a, int b){

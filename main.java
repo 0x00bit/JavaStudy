466a33d2308fb8ecf9b86c7309c199a189e16925
Magnus Junior <magnus.antonio.j@gmail.com>
2025-09-13 18:17:29 -0300

    feat: getname function

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 public class main{
     public static void main(String[] args) {
         int average = calculate(5, 10);
-        System.out.println("The average is: " + average);
+        String name = getName();
+        String message = "Hello, " + name + "! The average is: " + average;
+        System.out.println(message);
     }
+}
+
+public class getName(){
+    return "Steve";
 }
 
 public class calculate(int a, int b){

f7a89e7681495ea997fc414065f8261c08f00100
Magnus Junior <magnus.antonio.j@gmail.com>
2025-09-13 18:06:31 -0300

    add: calculate average

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main{
+    public static void main(String[] args) {
+        int average = calculate(5, 10);
+        System.out.println("The average is: " + average);
+    }
+}
+
+public class calculate(int a, int b){
+    return (a + b)/2;
+}
